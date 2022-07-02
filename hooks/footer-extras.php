d653c39ad3248b6f1b5d8b56a527a9c6408ee04a
chehin <chehinjuan@gmail.com>
2022-07-02 16:09:34 -0300

    first commit

---
+++ hooks/footer-extras.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong>BiblioTec - Sistema de Biblioteca&nbsp;<a href="https://compubinario.com"></a>.</strong> All rights
+    reserved.
+  </footer>
